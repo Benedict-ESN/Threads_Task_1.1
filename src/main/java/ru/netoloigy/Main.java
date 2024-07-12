85491d3a1237a0a1e5cc6219e49aa20b0a8d264c
Benedict.ESN <elder@elderscrolls.net>
2024-07-12 23:22:48 +0300

    Использование Callable и Future для нахождения максимального интервала значений

--- src/main/java/ru/netoloigy/Main.java
+++ src/main/java/ru/netoloigy/Main.java
@@ -3,10 +3,11 @@ package ru.netoloigy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
@@ -14,10 +15,11 @@ public class Main {
 
         long startTs = System.currentTimeMillis(); // start time
 
-        List<Thread> threads = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> futures = new ArrayList<>();
 
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
+            Callable<Integer> task = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -31,23 +33,26 @@ public class Main {
                                 break;
                             }
                         }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
+                        if (!bFound && maxSize < j - i) maxSize = j - i;
                     }
                 }
                 System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(thread);
-            thread.start();
+                return maxSize;
+            };
+            futures.add(executor.submit(task));
         }
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        int globalMaxSize = 0;
+        for (Future<Integer> future : futures) {
+            int result = future.get();
+            if (result > globalMaxSize) {
+                globalMaxSize = result;
+            }
         }
 
         long endTs = System.currentTimeMillis(); // end time
-
         System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Max interval size: " + globalMaxSize);
+        executor.shutdown();
     }
 
     public static String generateText(String letters, int length) {
